09666ae1953b136920e7505a8b0bf1e5ee28397f
James <james.jasenia@gmail.com>
2024-09-14 08:07:52 +1000

    Created `signin` router.

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
 
 const app = express();
 app.use(json());
 app.use(currentUserRouter);
+app.use(signinRouter);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
