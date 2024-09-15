78344ab5c20f432304929014896eced09297a9af
James <james.jasenia@gmail.com>
2024-09-16 07:24:01 +1000

    Added UserDoc interface

--- auth-service/src/models/user.ts
+++ auth-service/src/models/user.ts
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
 
 // Typescript's type checking and Mongoose require a bit of massaging.
-// An interface to provide type checking in a factory method.
+// An interface to provide type checking in a  User factory method.
 interface UserAttrs {
     email: string;
     password: string;
 }
 
-// An interface to provide additional type context to UserModel.
-interface UserModel extends mongoose.Model<any> {
-    build(attrs: UserAttrs): any;
+// An interface to provide additional type context to User Model.
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties that a User Document has
+// Mongoose may add additional properties to a document. We want some type checking on them.
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
 }
 
 const userSchema = new mongoose.Schema({
@@ -27,6 +34,6 @@ userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
 };
 
-const User = mongoose.model<any, UserModel>('User', userSchema);
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
 export default User;
