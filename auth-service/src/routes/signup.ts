6b7bb35dd1ad93161d507c523f20a9028d06967f
James <james.jasenia@gmail.com>
2024-09-16 07:26:18 +1000

    Added record creation and validation for signup route.

--- auth-service/src/routes/signup.ts
+++ auth-service/src/routes/signup.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import User from '../models/user';
+import RequestValidationError from '../errors/request-validation-error';
+import BadRequestError from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -13,14 +16,25 @@ const validator = [
         .withMessage('Password must be between 4 and 20 characters'),
 ]
 
-router.post('/api/users/signup', validator, (req: Request, res: Response) => {
+router.post('/api/users/signup', validator, async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        return res.status(400).send(errors.array())
-    } else {
-        res.send({});
+        throw new RequestValidationError(errors.array());
+    } 
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
+        throw new BadRequestError('Email already in use');
     }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
 });
 
 export { router as signupRouter };
