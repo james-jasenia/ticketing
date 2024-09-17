80015cc83777e0c17040d972f965ef19470fa620
James <james.jasenia@gmail.com>
2024-09-17 21:07:00 +1000

    Updated express instance config to handle cookies

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -9,10 +10,18 @@ import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 
 import errorHandler from './middlewares/error-handler';
+
 import NotFoundError from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true); // ingress-nginx is acting as a proxy
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+)
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
