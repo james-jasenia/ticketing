5a55d994c069e9efc7a3518b7f73ecfade178881
James <james.jasenia@gmail.com>
2024-09-17 21:06:28 +1000

    Added JWT generation

--- auth-service/src/routes/signup.ts
+++ auth-service/src/routes/signup.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import User from '../models/user';
+
 import RequestValidationError from '../errors/request-validation-error';
 import BadRequestError from '../errors/bad-request-error';
 
@@ -33,6 +36,15 @@ router.post('/api/users/signup', validator, async (req: Request, res: Response) 
 
     const user = User.build({ email, password });
     await user.save();
+
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, 'anySecretKey');
+
+    req.session = {
+        jwt: userJwt
+    };
 
     res.status(201).send(user);
 });
