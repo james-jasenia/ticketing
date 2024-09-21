af3ec6a64cd19bce5da03929bd2fa60f93d466fe
James <james.jasenia@gmail.com>
2024-09-22 07:35:24 +1000

    Added 201 return specs

--- auth-service/src/routes/__test__/signup.test.ts
+++ auth-service/src/routes/__test__/signup.test.ts
@@ -73,3 +73,41 @@ it('returns a 400 on duplicate email', async () => {
         })
         .expect(400)
 });
+
+it('sets a cookie after successful signup', async () => {
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test@test.com",
+            password: 'password'
+        })
+        .expect(201)
+
+    expect(response.get('Set-Cookie')).toBeDefined();
+});
+
+it('returns email in the body after successful signup', async () => {
+    const expectedEmail = "test@test.com"
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: expectedEmail,
+            password: 'password'
+        })
+        .expect(201)
+
+    expect(response.body.email).toEqual(expectedEmail);
+});
+
+it('returns an id in the body after successful signup', async () => {
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: "test@test.com",
+            password: 'password'
+        })
+        .expect(201)
+
+    expect(response.body.id).toBeDefined();
+});
