1229c50180e5a852e4a069469c7d54fb7367e6e4
James <james.jasenia@gmail.com>
2024-09-23 06:34:52 +1000

    Cleaned up currnet user specs

--- auth-service/src/routes/__test__/currentuser.test.ts
+++ auth-service/src/routes/__test__/currentuser.test.ts
@@ -1,8 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
 import signUpReturnCookie from '../../test/signun-return-cookie-helper';
-import { response } from 'express';
-
 
 it('responds with details about the current user', async () => {
     const cookie = await signUpReturnCookie();
@@ -27,12 +25,12 @@ it('responds with null if the user is not authenticated', async () => {
 
 it('response is case-sensitive (currentUser, not currentuser)', async () => {
     const cookie = await signUpReturnCookie();
-  
+
     const response = await request(app)
-      .get('/api/users/currentuser')
-      .set('Cookie', cookie)
-      .expect(200);
-  
+        .get('/api/users/currentuser')
+        .set('Cookie', cookie)
+        .expect(200);
+
     expect(response.body.currentUser).toBeDefined();
     expect(response.body.currentuser).toBeUndefined();
-  });
+});
