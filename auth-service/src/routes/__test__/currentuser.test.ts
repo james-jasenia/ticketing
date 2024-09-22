e124809a14bbe8ac84e4282647bd4bcddf251343
James <james.jasenia@gmail.com>
2024-09-22 12:28:31 +1000

    Added 'responds with null if the user is not authenticated' specs.

--- auth-service/src/routes/__test__/currentuser.test.ts
+++ auth-service/src/routes/__test__/currentuser.test.ts
@@ -1,6 +1,7 @@
 import request from 'supertest';
 import { app } from '../../app';
 import signUpReturnCookie from '../../test/signun-return-cookie-helper';
+import { response } from 'express';
 
 it('responds with details about the current user', async () => {
     const cookie = await signUpReturnCookie();
@@ -14,3 +15,12 @@ it('responds with details about the current user', async () => {
     expect(response.body.currentUser.email).toEqual('test@test.com');
     expect(response.body.currentUser.id).toBeDefined();
 });
+
+it('responds with null if the user is not authenticated', async () => {
+    const response = await request(app)
+        .get('/api/users/currentuser')
+        .expect(200);
+
+    expect(response.body.currentuser).toBeUndefined();
+});
+
