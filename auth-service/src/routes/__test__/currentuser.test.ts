83ba308be2f885eaa1c724f106dfa5e268ba2850
James <james.jasenia@gmail.com>
2024-09-22 12:24:22 +1000

    Added signUpReturnCookie test helper function.

--- auth-service/src/routes/__test__/currentuser.test.ts
+++ auth-service/src/routes/__test__/currentuser.test.ts
@@ -1,27 +1,16 @@
 import request from 'supertest';
 import { app } from '../../app';
+import signUpReturnCookie from '../../test/signun-return-cookie-helper';
 
 it('responds with details about the current user', async () => {
+    const cookie = await signUpReturnCookie();
+
     const response = await request(app)
-        .post('/api/users/signup')
-        .send({
-            email: "test@test.com",
-            password: 'password'
-        })
-        .expect(201)
-
-    const cookie = response.get('Set-Cookie')
-
-    if (!cookie) {
-        throw new Error("Cookie not set after signup");
-    }
-
-    const response2 = await request(app)
         .get('/api/users/currentuser')
         .set('Cookie', cookie)
         .send()
         .expect(200);
 
-    expect(response2.body.currentUser.email).toEqual('test@test.com');
-    expect(response2.body.currentUser.id).toBeDefined();
+    expect(response.body.currentUser.email).toEqual('test@test.com');
+    expect(response.body.currentUser.id).toBeDefined();
 });
---
+++ auth-service/src/test/signun-return-cookie-helper.ts
@@ -0,0 +1,25 @@
+import request from 'supertest';
+import { app } from './../app';
+
+async function signUpReturnCookie(
+    email = 'test@test.com',
+    password = 'test1234!'
+) {
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: email,
+            password: password
+        })
+        .expect(201)
+
+    const cookie = response.get('Set-Cookie')
+
+    if (!cookie) {
+        throw new Error("Cookie not set after signup");
+    }
+
+    return cookie
+}
+
+export default signUpReturnCookie;
