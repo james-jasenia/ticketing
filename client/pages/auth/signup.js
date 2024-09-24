b3d6418b4d70d6fccc4b592681d57060241b6d43
James <james.jasenia@gmail.com>
2024-09-25 06:41:09 +1000

    Added unhappy path for signup component

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -4,13 +4,18 @@ import axios from "axios";
 function SignupPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
 
-        const response = await axios.post('/api/users/signup', { email, password });
-
-        console.log(response.data);
+        try {
+            const response = await axios.post('/api/users/signup', { email, password });
+            console.log(response.data);
+        } catch (error) {   
+            setErrors(error.response.data.errors);
+            console.log(errors);
+        }
     }
 
     return(
@@ -24,6 +29,14 @@ function SignupPage() {
                 <label>Password</label>
                 <input type="password" className="form-control" value={password} onChange={e => setPassword(e.target.value)} />
             </div>
+            {errors.length > 0 && <div className="alert alert-danger">
+                <h4>Oooops...</h4>
+                <ul className="my-0">
+                    {errors.map((error, index) => (
+                        <li key={index}>{error.message}</li>
+                    ))}
+                </ul>
+            </div>}
             <button className="btn btn-primary">Sign Up</button>
         </form>
     )
