7b4360cc19510c95224ab4677a5d107cb0cdc903
James <james.jasenia@gmail.com>
2024-10-10 21:52:12 +1100

    Defined abstract class Listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,11 +1,10 @@
-import nats from 'node-nats-streaming';
+import nats, { Stan } from 'node-nats-streaming';
 import { Message } from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
 
 console.clear();
 
 const subject = 'ticketing:created'
-const queueGroup = 'ticketing:created:listener-queue-group'
 
 // Alternatively called stan (nats backwards)
 const client = nats.connect(
@@ -16,15 +15,23 @@ const client = nats.connect(
     }
 );
 
-const options = client.subscriptionOptions()
-    .setManualAckMode(true); // Set Manual Acknowledgement Mode = True - This means that you will need to manually acknowledge the message otherwise NATs will keep publishing.
+const options = client
+    .subscriptionOptions()
+    .setManualAckMode(true) // Set Manual Acknowledgement Mode = True - This means that you will need to manually acknowledge the message otherwise NATs will keep publishing.
+    .setDeliverAllAvailable() // The first time a service is created, it will receive all of the historical events
+    .setDurableName('') // This will keep a record of the events that have and have not been ackowledged by the subscriber
 
 client.on('connect', () => {
     console.log("listener connected to nats");
 
+    client.on('close', () => {
+        console.log('NATS close')
+        process.exit();
+    });
+
     const subscription = client.subscribe(
         subject, 
-        queueGroup,
+        'queue-group', // By having this queue group here, if there is an interuption in a subscription, nats will no longer through out the durable record
         options
     )
 
@@ -38,3 +45,53 @@ client.on('connect', () => {
        msg.ack() // Acknowledge the emissions to stop NATs from fallback publishing.
     });
 }); 
+
+
+// Close event to prevent NATs from hoping for a restart / recovery.
+process.on('SIGINT', () => client.close());
+process.on('SIGTERM', () => client.close());
+
+abstract class Listener {
+    
+    abstract subject: string;
+    abstract queueGroupName: string;
+    private client: Stan;
+    abstract onMessage(data: any, msg: Message): void;
+    protected ackWait = 5 * 1000
+    
+
+    constructor(client: Stan) {
+        this.client = client
+    }
+
+    subscriptionOptions() {
+        return this.client
+        .subscriptionOptions()
+        .setDeliverAllAvailable()
+        .setManualAckMode(true)
+        .setAckWait(this.ackWait)
+        .setDurableName(this.queueGroupName);
+    }
+
+    listen() {
+        const subscription = this.client.subscribe(
+            this.subject,
+            this.queueGroupName,
+            this.subscriptionOptions()
+        )
+
+        subscription.on('message', (msg: Message) => {
+            console.log(`Message received ${this.subject}, ${this.queueGroupName}`)
+
+            const parsedData = this.parseMessage(msg);
+            this.onMessage(parsedData, msg)
+        });
+    }
+
+    parseMessage(msg: Message) {
+        const data = msg.getData();
+        return typeof data === 'string' 
+        ? JSON.parse(data)
+        : JSON.parse(data.toString('utf8'));
+    }
+}
