f985a4cecd239c867f8aa06993d444dcd90e3825
James <james.jasenia@gmail.com>
2024-10-20 12:19:20 +1100

    Updated create-order.ts route

--- orders-service/src/routes/create-order.ts
+++ orders-service/src/routes/create-order.ts
@@ -1,8 +1,18 @@
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@jjgittix/common";
 
 const router = express.Router()
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+const validators = [
+    body('ticket')
+        .not()
+        .isEmpty()
+        .withMessage('Ticket ID must be provided.'),
+    
+]
+
+router.post('/api/orders', requireAuth, validators, validateRequest, async (req: Request, res: Response) => {
     res.send({})
 })
 
