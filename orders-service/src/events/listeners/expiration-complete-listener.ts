4e2732337b980850d494a136937c0532de931cc6
James <james.jasenia@gmail.com>
2024-11-01 06:40:35 +1100

    Created expiration-service

---
+++ orders-service/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,37 @@
+import { Listener, OrderStatus, Subjects } from "@jjgittix/common";
+import { ExpirationCompleteEvent } from "@jjgittix/common";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+import OrderCancelledPublisher from "../publishers/order-cancelled-publisher";
+import { natsWrapper } from "../../nats-wrapper";
+
+class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+    queueGroupName: string = this.queueGroupName;
+    subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
+
+    async onMessage(data: ExpirationCompleteEvent['data'], msg: Message): Promise<void> {
+        const order = await Order.findById(data.orderId).populate('ticket');
+
+        if (!order) {
+            throw new Error('Order not found')
+        }
+
+        order.set({
+            status: OrderStatus.Cacnelled
+        })
+
+        await order.save();
+
+        await new OrderCancelledPublisher(natsWrapper.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        })
+
+        msg.ack();
+    }
+}
+
+export default ExpirationCompleteListener
